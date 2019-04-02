b8b596f6526aeec73f189a5b5d71644e3272dc8c
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2019-04-02 23:21:49 +0300

    Bumped version to 0.1.3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vladiibine/describe_json',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
     entry_points={
         'console_scripts':[
